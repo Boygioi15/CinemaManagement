978ae120be91c84a3de38bccfaf5f706a0c942ba
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-12-24 11:36:58 +0700

    Call api film

--- frontend-client/src/Components/FilmInfoSection.jsx
+++ frontend-client/src/Components/FilmInfoSection.jsx
@@ -1,33 +1,26 @@
-const FilmInfoSection = ({ className }) => {
-    return (
-      <div className={className}>
-        <div>
-          <h2 className="font-interBold">MÔ TẢ</h2>
-          <div className="flex flex-col items-start justify-start film-description mt-3">
-            <p>Đạo diễn: Khương Ngọc</p>
-            <p>
-              Diễn viên: Việt Hương, Hồng Đào, Lê Khánh, Đinh Y Nhung, Ngọc Trinh
-            </p>
-            <p>Khởi chiếu: Thứ Sáu, 13/12/2024</p>
-          </div>
-        </div>
-        <div className="mt-8">
-          <h2 className="font-interBold">NỘI DUNG PHIM</h2>
-          <div className="flex flex-col items-start justify-start film-description mt-3">
-            <p>
-              Chuyện bắt đầu khi bà Nhị - con dâu cả của gia đình quyết định nhân
-              dịp đám giỗ của mẹ chồng, tụ họp cả bốn chị em gái - con ruột trong
-              nhà lại để thông báo chuyện sẽ tự bỏ tiền túi ra sửa sang căn nhà từ
-              đường cũ kỹ trước khi bão về. Vấn đề này khiến cho nội bộ gia đình
-              bắt đầu có những lục đục, chị dâu và các em chồng cũng xảy ra mâu
-              thuẫn, bất hoà. Dần dà những sự thật đằng sau việc "bằng mặt mà
-              không bằng lòng" giữa các chị em cũng dần được hé lộ, những bí mật,
-              nỗi đau sâu
-            </p>
-          </div>
+const FilmInfoSection = ({ className, filmDescription, filmContent }) => {
+  // Function to split description into lines
+  const renderDescription = (text) => {
+    // Split text by line breaks (assuming "\n" represents line breaks)
+    return text.split("\n").map((line, index) => <p key={index}>{line}</p>);
+  };
+
+  return (
+    <div className={className}>
+      <div>
+        <h2 className="font-interBold">MÔ TẢ</h2>
+        <div className="flex flex-col text-lg items-start justify-start mt-3">
+          {renderDescription(filmDescription)}
         </div>
       </div>
-    );
-  };
-  
-  export default FilmInfoSection;
+      <div className="mt-8">
+        <h2 className="font-interBold">NỘI DUNG PHIM</h2>
+        <div className="flex flex-col text-lg items-start justify-start mt-3">
+          {filmContent}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilmInfoSection;
--- frontend-client/src/Components/filmCard/index.jsx
+++ frontend-client/src/Components/filmCard/index.jsx
@@ -10,9 +10,42 @@ import CustomButton from "../button";
 import TrailerModal from "../TrailerModal";
 import { useState } from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
-const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing }) => {
+const FilmCard = ({
+  filmId,
+  imageUrl,
+  name,
+  country,
+  type,
+  duration,
+  ageLimit,
+  isShowing,
+  voice,
+  trailerURL,
+  twoDthreeD, // Array for 2D or 3D formats
+}) => {
+  const linkDetail = `/movie/detail/${filmId}`;
+  const navigate = useNavigate();
   const [videoOpen, setVideoOpen] = useState(false);
+
+  // Logic for Age Limit
+  const getAgeLabel = () => {
+    if (ageLimit === "T13" || ageLimit === "T16") {
+      return "TEEN";
+    }
+    if (ageLimit === "T18") {
+      return "ADULT";
+    }
+    if (ageLimit === "P" || ageLimit === "K") {
+      return "KID";
+    }
+    return ""; // Default case
+  };
+
+  const handleClickOverlay = () => {
+    navigate(linkDetail);
+  };
 
   return (
     <div className="flex flex-wrap justify-center gap-5 p-2">
@@ -21,42 +54,57 @@ const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing
         {/* FilmCard with hover effects */}
         <div className="relative border cursor-pointer border-gray-300 rounded-lg overflow-hidden shadow-md transition-transform duration-300 sm:hover:shadow-lg aspect-[2/3] w-full">
           {/* Film image */}
-          <img src={imageUrl} alt={name} className="w-full h-full object-cover" />
+          <img
+            src={imageUrl}
+            alt={name}
+            className="w-full h-full object-cover"
+          />
 
-          {/* Labels for 2D and Age Limit */}
+          {/* Labels for 2D, 3D and Age Limit */}
           <div className="absolute top-0 left-0 flex items-center transition-transform duration-300 ease-in-out transform sm:group-hover:-translate-y-full">
             <div className="flex items-center transition-transform duration-300 ease-in-out transform">
-              <div className="flex bg-[#FF9933]  w-[39px] h-[45px] justify-center items-center shadow-md">
-                <span className="border-2 border-black p-0.5 text-xs rounded-md font-interBold text-black">
-                  2D
-                </span>
-              </div>
+              {/* Conditionally render 2D */}
+              {twoDthreeD.includes("2D") && (
+                <div className="flex bg-[#FF9933] w-[39px] h-[45px] justify-center items-center shadow-md">
+                  <span className="border-2 border-black p-0.5 text-xs rounded-md font-interBold text-black">
+                    2D
+                  </span>
+                </div>
+              )}
+
+              {/* Conditionally render 3D */}
+              {twoDthreeD.includes("3D") && (
+                <div className="flex bg-[#663399] w-[39px] h-[45px] justify-center items-center shadow-md">
+                  <span className="border-2 border-white p-0.5 text-xs rounded-md font-interBold text-white">
+                    3D
+                  </span>
+                </div>
+              )}
+
+              {/* Age Limit */}
               <div className="flex flex-col w-[39px] h-[45px] items-center justify-center bg-[#FF0033] shadow-md">
                 <span className="text-white font-interBold overflow-hidden text-sm">
-                  T{ageLimit}
+                  {ageLimit}
                 </span>
-                {ageLimit < 18 ? (
-                  <span className="px-0.5 bg-black text-white text-[0.5rem] tracking-widest">
-                    TEEN
-                  </span>
-                ) : (
-                  <span className="px-0.5 bg-black text-white text-[0.5rem] tracking-widest">
-                    ADULT
-                  </span>
-                )}
+                <span className="px-0.5 bg-black text-white text-[0.5rem] tracking-widest">
+                  {getAgeLabel()}
+                </span>
               </div>
             </div>
           </div>
 
           {/* Overlay text */}
-          <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 transition-opacity duration-300 hidden sm:flex justify-center items-center text-center sm:group-hover:opacity-100 content-text">
+          <div
+            className="absolute inset-0 z-1 bg-black bg-opacity-70 opacity-0 transition-opacity duration-300 hidden sm:flex justify-center items-center text-center sm:group-hover:opacity-100 content-text"
+            onClick={() => handleClickOverlay()}
+          >
             <div className="flex flex-col items-start justify-start px-[38px] space-y-4 text-left w-full">
               <h3 className="mb-3 text-lg">
-                {name}: NHIỆM VỤ GIẢI CỨU HOÀNG GIA (P) {ageLimit}
+                {name} ({ageLimit})
               </h3>
               <p className="flex items-center mt-2">
                 <FaTag className="w-5 h-5 mr-2 align-middle" />
-                Thể loại: {type}
+                {type}
               </p>
               <p className="flex items-center mt-2">
                 <FaRegClock className="w-5 h-5 mr-2 align-middle" />
@@ -64,11 +112,11 @@ const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing
               </p>
               <p className="flex items-center mt-2">
                 <FaGlobeAmericas className="w-5 h-5 mr-2 align-middle" />
-                Quốc gia: {country}
+                {country}
               </p>
               <p className="flex items-center mt-2">
                 <FaCommentDots className="w-5 h-5 mr-2 align-middle" />
-                Phụ đề: {country}
+                {voice}
               </p>
             </div>
           </div>
@@ -76,8 +124,11 @@ const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing
 
         {/* Info Section */}
         <div className="w-full">
-          <a className="font-bold text-lg mt-3 h-[48px] overflow-hidden line-clamp-2 text-white text-center group-hover:text-mainColor cursor-pointer">
-            {name}: NHIỆM VỤ GIẢI CỨU HOÀNG GIA (P) {ageLimit}
+          <a
+            href={linkDetail}
+            className="font-bold text-lg mt-3 h-[48px] overflow-hidden line-clamp-2 text-white text-center group-hover:text-mainColor cursor-pointer"
+          >
+            {name} ({ageLimit})
           </a>
           <div className="flex justify-between mt-3 pt-4 button-font">
             {/* Trailer button for larger screens */}
@@ -93,7 +144,7 @@ const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing
             <TrailerModal
               videoOpen={videoOpen}
               setVideoOpen={setVideoOpen}
-              videoUrl="https://www.youtube-nocookie.com/embed/WHmHpntEMbk?controls=1&enablejsapi=1&rel=0&fs=1"
+              videoUrl={trailerURL}
             />
 
             {/* Custom button */}
@@ -103,9 +154,11 @@ const FilmCard = ({ imageUrl, name, country, type, duration, ageLimit, isShowing
               gradientTo="#3366CC" /* Gradient end color */
               textColor="#000000" /* Text color */
               hoverTextColor="#FFFFFF" /* Text color on hover */
-              href="https://example.com" /* Link */
+              href={linkDetail}
               className="w-full h-[40px] sm:w-[119px]"
-              text={isShowing ? "Đặt vé" : "Tìm hiểu thêm"} /* Conditional text */
+              text={
+                isShowing ? "Đặt vé" : "Tìm hiểu thêm"
+              } /* Conditional text */
             />
           </div>
         </div>
