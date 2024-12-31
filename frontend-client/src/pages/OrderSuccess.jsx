c351b1b5f75e47f0a35bd6326a686a9e87b84504
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2025-01-01 00:53:01 +0700

    fix order success

--- frontend-client/src/pages/OrderSuccess.jsx
+++ frontend-client/src/pages/OrderSuccess.jsx
@@ -5,19 +5,19 @@ const OrderSuccessPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 mt-10">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-center">
-        <h2 className="text-2xl font-semibold text-green-500">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-white p-12 rounded-lg shadow-lg w-full max-w-lg text-center">
+        <h2 className="text-3xl font-semibold text-green-500">
           Đơn hàng thành công!
         </h2>
-        <p className="text-lg text-gray-600 mt-4">
+        <p className="text-xl text-gray-600 mt-4">
           Cảm ơn bạn đã đặt hàng! Vui lòng kiểm tra email của bạn để xem hóa
           đơn.
         </p>
-        <div className="mt-6">
+        <div className="mt-8">
           <button
             onClick={() => navigate("/")}
-            className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition duration-300"
+            className="bg-blue-500 text-white py-3 px-8 rounded-lg hover:bg-blue-600 transition duration-300 text-xl"
           >
             Quay về trang chủ
           </button>
