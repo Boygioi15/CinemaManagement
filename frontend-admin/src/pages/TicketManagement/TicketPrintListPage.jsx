75ff6022c716effb4dffce7b0adc50d968ba12d4
Boygioi15 <boygioi85@gmail.com>
2025-01-08 21:35:54 +0700

    fix ticket

--- frontend-admin/src/pages/TicketManagement/TicketPrintListPage.jsx
+++ frontend-admin/src/pages/TicketManagement/TicketPrintListPage.jsx
@@ -233,7 +233,7 @@ const TicketPrintListPage = () => {
   const filteredData = useMemo(() => {
     let filtered = orders.filter((order) => {
       const matchesDate = selectedDate
-        ? order.filmShow.showDate &&
+        ? order.filmShow && order.filmShow.showDate &&
           new Date(order.filmShow.showDate).toLocaleDateString() ===
             new Date(selectedDate).toLocaleDateString()
         : true;
@@ -549,23 +549,6 @@ const TicketPrintListPage = () => {
           >
             Xóa lọc
           </button>
-        </div>
-        <div className="ml-20 flex items-center gap-4">
-          <span className="text-xl font-bold text-gray-800 ">Sắp xếp:</span>
-          <div className="relative inline-block w-64 ">
-            <select
-              value={sortOption}
-              onChange={(e) => setSortOption(e.target.value)}
-              className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            >
-              <option value="">Không sắp xếp</option>
-              {!selectedDate && <option value="Theo ngày">Theo ngày</option>}
-              {selectedDate && <option value="Theo giờ">Theo giờ</option>}
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <BsSortDown className="h-4 w-4" />
-            </div>
-          </div>
         </div>
       </div>
 
